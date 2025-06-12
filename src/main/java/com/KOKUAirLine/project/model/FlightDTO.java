21b647d65f30602adab5a147d57711b996ca6d52
yong10191 <seoyong1029@gmail.com>
2025-06-12 17:37:06 +0900

    17:37

--- src/main/java/com/KOKUAirLine/project/model/FlightDTO.java
+++ src/main/java/com/KOKUAirLine/project/model/FlightDTO.java
@@ -1,18 +1,18 @@
-package com.KOKUAirLine.project.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
-public class FlightSchedule {
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String departure;
-    private String arrival;
-    private String flightTime;
-
-}
+//package com.KOKUAirLine.project.model;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//
+//@Entity
+//public class FlightSchedule {
+//	
+//	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String departure;
+//    private String arrival;
+//    private String flightTime;
+//
+//}
