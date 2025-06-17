516e3854e148dcf3af2b08dd0985832186e6c956
서영 <globalinseoyeong@gmail.com>
2025-06-17 10:01:35 +0900

    Update ReservationCheckController.java

--- src/main/java/com/KOKUAirLine/project/controller/ReservationCheckController.java
+++ src/main/java/com/KOKUAirLine/project/controller/ReservationCheckController.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.KOKUAirLine.project.service.ReservationCheckService;
-
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class ReservationCheckController {
-   @Autowired
-   private ReservationCheckService service;
    
    @GetMapping("/reservationCheck")
    public String reservationCheck(
