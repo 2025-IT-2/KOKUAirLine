51c8833dd3da908883b2214e28dfe925372cc093
이상수 <bloodmaigeelfs@gmail.com>
2025-06-17 10:02:35 +0900

    .

--- src/main/java/com/KOKUAirLine/project/controller/ReservationCheckController.java
+++ src/main/java/com/KOKUAirLine/project/controller/ReservationCheckController.java
@@ -1,17 +1,12 @@
 package com.KOKUAirLine.project.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.KOKUAirLine.project.service.ReservationCheckService;
 
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class ReservationCheckController {
-	@Autowired
-	private ReservationCheckService service;
 	
 	@GetMapping("/reservationCheck")
 	public String reservationCheck(
