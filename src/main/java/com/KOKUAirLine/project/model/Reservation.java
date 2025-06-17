e8a2aa90fe67635c798098843dc2257e0282048f
doksakim7 <doksakim7@naver.com>
2025-06-17 12:42:36 +0900

    PaymentInfo DB 생성
    PaymentInfo DB 생성(PaymentInfo.java, PaymentInfoRepo.java)

--- src/main/java/com/KOKUAirLine/project/model/Reservation.java
+++ src/main/java/com/KOKUAirLine/project/model/Reservation.java
@@ -22,14 +22,14 @@ public class Reservation {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)	
 	@Column(name = "RESINUM")	
-	private Integer resiNum; 		//예약번호
+	private Long resiNum; 		//예약번호
 	
 	@ManyToOne
-	@JoinColumn(name = "FLIGHTNO") // Reservation 테이블의 외래키 컬럼명
-	private FlightInfo flightInfo;		//항공편명
+	@JoinColumn(name = "FLIGHTNO") 	// Reservation 테이블의 외래키 컬럼명
+	private FlightInfo flightInfo;	//항공편명
     
 	@Column(name = "FLIGHTMEAL")
-	private String FlightMeal;	  
+	private String FlightMeal;	  	//기내식
 		
 	@Column(name = "RESNUML")	
 	private Integer resNumL; 		//성인인원
@@ -41,7 +41,7 @@ public class Reservation {
 	private Integer resNumS; 		//유아인원
 	
 	@Column(name = "RESDATE")	
-	private LocalDate resDate;    		// 예매일자
+	private LocalDate resDate;    	// 예매일자
 	
 	@Column(name = "FLIGHTmEALYN")	
 	private String flightMealYN;	// 기내식신청여부
