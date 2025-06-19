7199a8ea3cc6fc54c4704ee4e04d23a35b4016f4
서영 <globalinseoyeong@gmail.com>
2025-06-19 13:46:16 +0900

    Update FlightInfoDataTest.java

--- src/test/java/com/KOKUAirLine/project/FlightInfoDataTest.java
+++ src/test/java/com/KOKUAirLine/project/FlightInfoDataTest.java
@@ -3,6 +3,8 @@ package com.KOKUAirLine.project;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
@@ -36,28 +38,50 @@ public class FlightInfoDataTest {
         InFlightMeal chicken = inFlightMealRepo.findAll().stream()
                 .filter(m -> m.getFlightMeal().equals("チキンライス"))
                 .findFirst().orElseThrow();
+        
+     // 1. LocalDateTime 만들기
+        LocalDateTime departure1 = LocalDateTime.of(2025, 7, 1, 3, 30); // 2025-07-01 03:30
+        LocalDateTime arrival1 = LocalDateTime.of(2025, 7, 1, 7, 15);   // 2025-07-01 07:15
+
+        // 2. LocalDateTime을 java.util.Date로 변환
+        Date depDate1 = Date.from(departure1.atZone(ZoneId.systemDefault()).toInstant());
+        Date arrDate1 = Date.from(arrival1.atZone(ZoneId.systemDefault()).toInstant());
+        
+        LocalDateTime departure2 = LocalDateTime.of(2025, 7, 4, 13, 30); // 2025-07-01 03:30
+        LocalDateTime arrival2 = LocalDateTime.of(2025, 7, 4, 17, 15);   // 2025-07-01 07:15
+        
+        // 2. LocalDateTime을 java.util.Date로 변환
+        Date depDate2 = Date.from(departure2.atZone(ZoneId.systemDefault()).toInstant());
+        Date arrDate2 = Date.from(arrival2.atZone(ZoneId.systemDefault()).toInstant());
 
         // 기내식 이름을 가져와서 사용
         FlightInfo flight1 = new FlightInfo(
             "KOKU123", beef, "東京",
-            new Date(), new Date(), 1,
-            "大阪", new Date(), new Date(), 2,
+            depDate1, depDate1, 1,
+            "大阪", arrDate1, arrDate1, 2,
             100, 180, 300
         );
 
         FlightInfo flight2 = new FlightInfo(
-            "KOKU456", chicken, "ソウル",
-            new Date(), new Date(), 1,
-            "福岡", new Date(), new Date(), 1,
+            "KOKU456", chicken, "大阪",
+            depDate2, depDate2, 1,
+            "東京", arrDate2, arrDate2, 1,
             75, 150, 301
         );
 
         FlightInfo flight3 = new FlightInfo(
             "KOKU789", beef, "札幌",
-            new Date(), new Date(), 2,
+            depDate2, depDate2, 2,
             "名古屋", new Date(), new Date(), 2,
             50, 120, 302
         );
+        
+        FlightInfo flight4 = new FlightInfo(
+        		"KOKU789", beef, "札幌",
+        		arrDate2, arrDate2, 2,
+        		"名古屋", new Date(), new Date(), 2,
+        		50, 120, 302
+        		);
 
         flightInfoRepo.save(flight1);
         flightInfoRepo.save(flight2);
